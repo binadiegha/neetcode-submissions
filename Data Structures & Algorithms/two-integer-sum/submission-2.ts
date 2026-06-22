ce81bce67302c60c1498e033f5f45b74ef11b26b
Jones Binadiegha Gabriel <jonesbgabriel@gmail.com>
2026-06-22 18:00:35 +0100

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+
+        let numbers: number[] = [];
+        let balance: number = target;
+        let active = 0;
+        let pass: boolean = false;
+
+        while (pass == false){
+            for (let i = 0; i < nums.length; i++){
+                if(i == active){
+                    balance = target - nums[i];
+                    continue;
+                }
+
+                if ((nums[i] - balance) == 0) {
+                    numbers = [active, i]
+                    pass = true;
+                    break;
+                } 
+            }
+
+            // Reset the balance and target
+            balance = target;
+            active++;
+        }
+        
+        return numbers;
+    }
+}
